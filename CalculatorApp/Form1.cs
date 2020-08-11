cde53a406019a3be20f0a0282f00e186b22e5cbb
siminarosioru <simina.rosioru@gmail.com>
2020-08-11 13:54:20 +0200

    Add project files.

---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class FormHomepage : Form
+    {
+        public FormHomepage()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonCalculatorForm_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
