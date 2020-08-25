39e327dd1de6d9b37f05e04d5e0db4c05772da5e
siminarosioru <simina.rosioru@gmail.com>
2020-08-25 15:19:10 +0200

    Exceptie: varianta if ... else

--- CalculatorApp/FormCalculator.cs
+++ CalculatorApp/FormCalculator.cs
@@ -64,20 +64,56 @@ namespace CalculatorApp
             decimal termen1 = numericUpDownTermen1.Value;
             decimal termen2 = numericUpDownTermen2.Value;
 
-            try
+            ////declararea variabilei locale + calcul
+            //decimal rezultat = termen1 / termen2;
+            ////afisarea rezultatului
+            //textBoxRezultat.Text = rezultat.ToString();
+
+            /* termen1 / 0 = eroare
+             * pentru ca impartirea la 0 nu are sens,
+             * in mod normal noi ar trebui sa ne gandim astfel:
+             * 0(termen2) * ? = termen1 => eroare
+             * dar 0(termen1) / termen2 = corect
+             */
+            //varianta if...else
+            if (termen2 == 0)
+            {
+                string message = "do not divide by 0";
+                string title = "divide by 0";
+                //afisarea + fixarea exceptiei
+                MessageBox.Show(message, title);
+                //MessageBox.Show(message);
+            }
+            else
             {
                 //declararea variabilei locale + calcul
                 decimal rezultat = termen1 / termen2;
                 //afisarea rezultatului
                 textBoxRezultat.Text = rezultat.ToString();
             }
-            catch
-            {
-                string message = "do not divide by 0";
-                string title = "divide by 0";
-                //afisarea + fixarea exceptiei
-                MessageBox.Show(message, title);
-            }
+            //sau varianta try ... catch
+            /* Exception e , e un fel de "root" (mama tuturor exceptiilor). 
+             * Se pot face mai multe catch-uri, cu diferite nivele de exceptie 
+             * (de la unul specific la cel mai larg)
+             * se poate face de genul try { .... } catch (DivideByZeroException e)
+             * {...} catch (Exception e)
+             */
+            //try
+            //{
+            //    //declararea variabilei locale + calcul
+            //    decimal rezultat = termen1 / termen2;
+            //    //afisarea rezultatului
+            //    textBoxRezultat.Text = rezultat.ToString();
+            //}
+            //catch
+            //{
+            //    string message = "do not divide by 0";
+            //    string title = "divide by 0";
+            //    //afisarea + fixarea exceptiei
+            //    MessageBox.Show(message, title);
+            //    //fixarea exceptiei
+            //    //MessageBox.Show(message);
+            //}
         }
     }
 }
